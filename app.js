12235d3f7e8b9c3fb2505e77df25022533021c3c
Pradeep Kumar <pradeepmishra914018@gmail.com>
2026-09-02 00:07:59 +0530

    app.js

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.use('/api',router)
 
 
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully");
+});
 
 const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
