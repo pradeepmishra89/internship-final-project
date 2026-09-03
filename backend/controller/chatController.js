c91df7701dbfe07f4de9af943d89c3b0402dd98e
Pradeep Kumar <pradeepmishra914018@gmail.com>
2026-09-03 13:47:50 +0530

    fix: Ollama Api

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -17,8 +17,9 @@ const groq = new Groq({
 // CONSTANTS
 // =====================================================
 
-const TEXT_MODEL = "llama3.2:latest";
+// const TEXT_MODEL = "openai/gpt-oss-20b";
 
+const TEXT_MODEL = "llama3.2:latest";
 const VISION_MODEL = "qwen/qwen3.6-27b";
 
 const MAX_HISTORY_MESSAGES = 10;
